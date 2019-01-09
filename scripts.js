e31cca8b583a776abeb6e6e0f694c278eae9bfed
Mansoor Zaman <manisureal@outlook.com>
2019-01-09 16:44:19 +0000

    created function getTextColorContrastYIQ  to calculate font color based on background color

--- scripts.js
+++ scripts.js
@@ -410,6 +410,14 @@ function medicationInformation(itemId, slotTime) {
   html+= '</div>'
 }
 
+function getTextColorContrastYIQ(hexcolor){
+  var r = parseInt(hexcolor.substr(0,2),16);
+  var g = parseInt(hexcolor.substr(2,2),16);
+  var b = parseInt(hexcolor.substr(4,2),16);
+  var yiq = ((r*299)+(g*587)+(b*114))/1000;
+  return (yiq >= 128) ? 'black' : 'white';
+}
+
 function todaysDoseTimes(){
   doseTimesHash = {}
   patient.todays_administrations.filter(x => x.item_id === 2454).forEach(function(a){
