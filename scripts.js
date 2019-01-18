d31c57269d581d83d5de2d2b82c7f1eae380c0ef
Mansoor Zaman <manisureal@outlook.com>
2019-01-18 15:45:50 +0000

    avoided duplication when creating a hash for todaysDoseTimes

--- scripts.js
+++ scripts.js
@@ -519,9 +519,9 @@ function todaysDoseTimes(itemId){
     patient.time_slots.forEach(function(ts){
       if (a.slot_time === ts.time) {
         if (doseTimesHash[a.item_id] == null) {
-          doseTimesHash[a.item_id] = [{"time":ts.time,"show_as":ts.show_as,"color":ts.color,"dose_presc":a.dose_prescribed}]
+          doseTimesHash[a.item_id] = {[ts.time]: {"time":ts.time,"show_as":ts.show_as,"color":ts.color,"dose_presc":a.dose_prescribed}}
         } else {
-          doseTimesHash[a.item_id].push({"time":ts.time,"show_as":ts.show_as,"color":ts.color,"dose_presc":a.dose_prescribed})
+          doseTimesHash[a.item_id][ts.time] = {"time":ts.time,"show_as":ts.show_as,"color":ts.color,"dose_presc":a.dose_prescribed}
         }
       }
     })
