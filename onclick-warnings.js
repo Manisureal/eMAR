bc9d80765d0b0add45a663aefae39cb639e8219d
Mansoor Zaman <manisureal@outlook.com>
2019-02-13 16:26:34 +0000

    removed _proto_ as this was breaking jquery

--- onclick-warnings.js
+++ onclick-warnings.js
@@ -5,7 +5,7 @@ function createParacetamolFlag(){
          item.mandatory_instructions.toLowerCase().includes('paracetamol')
        ) {
           console.log(item)
-          setTimeout(function(){ item.__proto__.is_paracetamol = true }, 100);
+          item['is_paracetamol'] = true
          }
   })
 }
