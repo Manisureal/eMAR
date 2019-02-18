c774a49f0d7ee5e993c3e33b3894f5ee63275c7d
Mansoor Zaman <manisureal@outlook.com>
2019-02-18 14:27:17 +0000

    created checkParacetamolAdminsToSend Fc

--- onclick-warnings.js
+++ onclick-warnings.js
@@ -9,3 +9,24 @@ function createParacetamolFlag(){
          }
   })
 }
+
+function checkParacetamolAdminsToSend(itemId,slotTime){
+  clickedItem = patient.this_cycle_items.find(x => x.id === itemId)
+  if (administrationsToSend.length > 0){
+    administrationsToSend.forEach((admin)=>{
+      tcAdminItem = patient.this_cycle_items.find(x => x.id === admin.item_id)
+        if (itemId === tcAdminItem.id){
+          if (tcAdminItem.is_paracetamol) {
+            paracetamolWarning()
+          }
+        } else if (clickedItem.is_paracetamol) {
+            paracetamolWarning()
+        }
+        else {
+          medicationAdministration(itemId,slotTime);
+        }
+      })
+  } else {
+    medicationAdministration(itemId,slotTime);
+  }
+}
